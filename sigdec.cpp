342d87a495686bf7fd2abc410847b41c84e0fc1e
Jack Whitham <jack.d.whitham@gmail.com>
2024-11-07 23:35:38 +0000

    Encapsulated type

--- sigdec.cpp
+++ sigdec.cpp
@@ -2,7 +2,7 @@
 //
 #include <stdio.h>
 #include <string.h>
-#include <stdint.h>
+#include <cstdint>
 #include <stdlib.h>
 #include <math.h>
 #include <limits.h>
@@ -19,47 +19,79 @@
 
 namespace {
 
-typedef struct my_filter_state_t {
-    double a0, a1, a2;
-    double b0, b1, b2;
-    double i1, i2;
-    double o1, o2;
-} my_filter_state_t;
+struct fixed_t {
+public:
+    fixed_t() : m_value(0.0) {}
+    fixed_t(const double value) : m_value(value) {}
+    fixed_t operator*(const fixed_t& other) {
+        return fixed_t(m_value * other.m_value);
+    }
+    fixed_t operator+(const fixed_t& other) {
+        return fixed_t(m_value + other.m_value);
+    }
+    fixed_t operator-(const fixed_t& other) {
+        return fixed_t(m_value - other.m_value);
+    }
+    fixed_t operator/(const fixed_t& other) {
+        return fixed_t(m_value / other.m_value);
+    }
+    bool operator>(const fixed_t& other) {
+        return m_value > other.m_value;
+    }
+    double to_double() {
+        return m_value;
+    }
+private:
+    double m_value;
+};
+
+struct my_filter_state_t {
+    my_filter_state_t() {};
+    fixed_t a0, a1, a2;
+    fixed_t b0, b1, b2;
+    fixed_t i1, i2;
+    fixed_t o1, o2;
+};
 
 static void my_filter_setup(
                 my_filter_state_t* mf,
                 t_header* header,
                 double frequency,
                 double width) {
-    memset(mf, 0, sizeof(my_filter_state_t));
     const double w0 = 2 * M_PI * frequency / (double) header->sample_rate;
     const double alpha = sin(w0)/(2*frequency/width);
-    mf->b0 =   alpha;
-    mf->b1 =   0;
-    mf->b2 =  -alpha;
-    mf->a0 =   1 + alpha;
-    mf->a1 =  -2*cos(w0);
-    mf->a2 =   1 - alpha;
-    mf->b2 /= mf->a0;
-    mf->b1 /= mf->a0;
-    mf->b0 /= mf->a0;
-    mf->a2 /= mf->a0;
-    mf->a1 /= mf->a0;
+    double b0 =   alpha;
+    double b1 =   0;
+    double b2 =  -alpha;
+    double a0 =   1 + alpha;
+    double a1 =  -2*cos(w0);
+    double a2 =   1 - alpha;
+    b2 /= a0;
+    b1 /= a0;
+    b0 /= a0;
+    a2 /= a0;
+    a1 /= a0;
     printf("Matrix for frequency %1.0f Hz width %1.0f Hz\n", frequency, width);
     printf("   w0 = %14.6e a  = %14.6e\n", w0, alpha);
-    printf("   a0 = %14.6e a1 = %14.6e a2 = %14.6e\n", mf->a0, mf->a1, mf->a2);
-    printf("   b0 = %14.6e b1 = %14.6e b2 = %14.6e\n", mf->b0, mf->b1, mf->b2);
+    printf("   a0 = %14.6e a1 = %14.6e a2 = %14.6e\n", a0, a1, a2);
+    printf("   b0 = %14.6e b1 = %14.6e b2 = %14.6e\n", b0, b1, b2);
+    mf->b0 = fixed_t(b0);
+    mf->b1 = fixed_t(b1);
+    mf->b2 = fixed_t(b2);
+    mf->a0 = fixed_t(a0);
+    mf->a1 = fixed_t(a1);
+    mf->a2 = fixed_t(a2);
 }
 
 static void my_filter(
                 my_filter_state_t* mf,
-                double* input,
-                double* output,
+                fixed_t* input,
+                fixed_t* output,
                 size_t num_samples) {
 
     for (size_t i = 0; i < num_samples; i++) {
-        double i0 = input[i];
-        double o0 = i0*mf->b0 + mf->i1*mf->b1 + mf->i2*mf->b2 - mf->o1*mf->a1 - mf->o2*mf->a2;
+        fixed_t i0 = input[i];
+        fixed_t o0 = i0*mf->b0 + mf->i1*mf->b1 + mf->i2*mf->b2 - mf->o1*mf->a1 - mf->o2*mf->a2;
         mf->i2 = mf->i1;
         mf->i1 = i0;
         mf->o2 = mf->o1;
@@ -68,17 +100,17 @@ static void my_filter(
     }
 }
 
-typedef struct rc_filter_state_t {
-    double      level;
-    double      decay;
-} rc_filter_state_t;
+struct rc_filter_state_t {
+    rc_filter_state_t() {};
+    fixed_t      level;
+    fixed_t      decay;
+};
 
 static void rc_filter_setup(
                     rc_filter_state_t* ds,
                     t_header* header,
-                    uint32_t frequency)
+                    std::uint32_t frequency)
 {
-    memset(ds, 0, sizeof(rc_filter_state_t));
     // Number of samples per bit
     const double bit_samples = ((double) header->sample_rate / (double) BAUD_RATE);
     // This is the time constant, like k = 1 / RC for a capacitor discharging
@@ -86,18 +118,18 @@ static void rc_filter_setup(
     // The level should be reduced from 1.0 to RC_DECAY_PER_BIT during each bit
     const double time_constant = log(RC_DECAY_PER_BIT) / -bit_samples;
     // Each transition from t to t+1 is a multiplication by exp(-k)
-    ds->decay = exp(-time_constant);
+    ds->decay = fixed_t(exp(-time_constant));
 }
 
 static void rc_filter(
                     rc_filter_state_t* ds,
-                    double* samples,
+                    fixed_t* samples,
                     size_t num_samples,
-                    double* levels)
+                    fixed_t* levels)
 {
     for (size_t i = 0; i < num_samples; i++) {
-        double sample = samples[i];
-        ds->level *= ds->decay;
+        fixed_t sample = samples[i];
+        ds->level = ds->level * ds->decay;
         if (sample > ds->level) {
             ds->level = sample;
         }
@@ -112,16 +144,16 @@ typedef enum { STOP, STOP_ERROR, START,
 
 
 typedef struct serial_decode_state_t {
-    uint32_t    sample_countdown, half_bit;
-    uint32_t    byte_countdown;
-    uint8_t     byte;
-    bit_t       previous_bit;
+    std::uint32_t   sample_countdown, half_bit;
+    std::uint32_t   byte_countdown;
+    std::uint8_t    byte;
+    bit_t           previous_bit;
 } serial_decode_state_t;
 
 static void serial_decode(
                     serial_decode_state_t* ds,
-                    double* upper_levels,
-                    double* lower_levels,
+                    fixed_t* upper_levels,
+                    fixed_t* lower_levels,
                     identified_t* identified,
                     bit_t* received_bit,
                     size_t num_samples,
@@ -129,13 +161,13 @@ static void serial_decode(
 {
     for (size_t i = 0; i < num_samples; i++) {
         bit_t bit = ds->previous_bit;
-        double sum = upper_levels[i] + lower_levels[i];
-        if (sum < MINIMUM_AMPLITUDE) {
+        fixed_t sum = upper_levels[i] + lower_levels[i];
+        if (fixed_t(MINIMUM_AMPLITUDE) > sum) {
             bit = INVALID;
-        } else if ((upper_levels[i] > (sum * SCHMITT_THRESHOLD))
+        } else if ((upper_levels[i] > (sum * fixed_t(SCHMITT_THRESHOLD)))
         && ((bit == ZERO) || (bit == INVALID))) {
             bit = ONE;
-        } else if ((lower_levels[i] > (sum * SCHMITT_THRESHOLD))
+        } else if ((lower_levels[i] > (sum * fixed_t(SCHMITT_THRESHOLD)))
         && ((bit == ONE) || (bit == INVALID))) {
             bit = ZERO;
         }
@@ -213,14 +245,14 @@ static void generate(FILE* fd_in, FILE* fd_out, FILE* fd_debug)
     || (header.bytes_per_second != (header.sample_rate * header.bytes_per_period))
     || (header.bits_per_sample != ((header.bytes_per_period / header.number_of_channels) * 8))
     || (memcmp(header.fixed_data, "data", 4) != 0)
-    || (header.bytes_per_period != sizeof(int16_t))
+    || (header.bytes_per_period != sizeof(std::int16_t))
     || (header.bits_per_sample != 16)) {
         fprintf(stderr, "Format is incorrect - needs to be a simple mono PCM file, 16 bit\n");
         exit(1);
     }
 
     // Check endianness
-    if (((uint32_t *) &header.fixed_riff)[0] != 0x46464952) {
+    if (((std::uint32_t *) &header.fixed_riff)[0] != 0x46464952) {
         fprintf(stderr, "endianness error (little endian assumed, sorry)\n");
         exit(1);
     }
@@ -230,7 +262,8 @@ static void generate(FILE* fd_in, FILE* fd_out, FILE* fd_debug)
     my_filter_setup(&upper_filter, &header, UPPER_FREQUENCY, FILTER_WIDTH);
     my_filter_setup(&lower_filter, &header, LOWER_FREQUENCY, FILTER_WIDTH);
 
-    rc_filter_state_t upper_decode_state, lower_decode_state;
+    rc_filter_state_t upper_decode_state;
+    rc_filter_state_t lower_decode_state;
     rc_filter_setup(&upper_decode_state, &header, UPPER_FREQUENCY); 
     rc_filter_setup(&lower_decode_state, &header, LOWER_FREQUENCY); 
 
@@ -242,28 +275,28 @@ static void generate(FILE* fd_in, FILE* fd_out, FILE* fd_debug)
     uint64_t sample_count = 0;
 
     while (1) {
-        int16_t samples[BLOCK_SIZE];
-        double input[BLOCK_SIZE];
-        double upper_output[BLOCK_SIZE];
-        double lower_output[BLOCK_SIZE];
+        std::int16_t samples[BLOCK_SIZE];
+        fixed_t input[BLOCK_SIZE];
+        fixed_t upper_output[BLOCK_SIZE];
+        fixed_t lower_output[BLOCK_SIZE];
         size_t i;
 
         // Input from .wav file
-        ssize_t num_samples = fread(samples, sizeof(int16_t), BLOCK_SIZE, fd_in);
+        ssize_t num_samples = fread(samples, sizeof(std::int16_t), BLOCK_SIZE, fd_in);
         if (num_samples <= 0) {
             break;
         }
 
         for (i = 0; i < ((size_t) num_samples); i++) {
-            input[i] = (double) samples[i] / (double) INT16_MAX;
+            input[i] = (fixed_t) samples[i] / (fixed_t) INT16_MAX;
         }
         // Bandpass filters
         my_filter(&upper_filter, input, upper_output, num_samples);
         my_filter(&lower_filter, input, lower_output, num_samples);
 
         // Level
-        double upper_levels[BLOCK_SIZE];
-        double lower_levels[BLOCK_SIZE];
+        fixed_t upper_levels[BLOCK_SIZE];
+        fixed_t lower_levels[BLOCK_SIZE];
         rc_filter(&upper_decode_state, upper_output, (size_t) num_samples, upper_levels);
         rc_filter(&lower_decode_state, lower_output, (size_t) num_samples, lower_levels);
 
@@ -281,11 +314,11 @@ static void generate(FILE* fd_in, FILE* fd_out, FILE* fd_debug)
         // Debug output shows filtering and detection
         for (i = 0; i < ((size_t) num_samples); i++) {
             fprintf(fd_debug, "%7.5f ", (double) sample_count / (double) header.sample_rate); // time
-            fprintf(fd_debug, "%7.4f ", (double) input[i] / (double) INT_MAX); // encoded signal
-            fprintf(fd_debug, "%7.4f ", (double) upper_output[i] / (double) INT_MAX); // upper bandpass filter output
-            fprintf(fd_debug, "%7.4f ", (double) lower_output[i] / (double) INT_MAX); // lower bandpass filter output
-            fprintf(fd_debug, "%7.4f ", upper_levels[i]); // upper, rectify and RC filter
-            fprintf(fd_debug, "%7.4f ", lower_levels[i]); // lower, rectify and RC filter
+            fprintf(fd_debug, "%7.4f ", input[i].to_double() / (double) INT_MAX); // encoded signal
+            fprintf(fd_debug, "%7.4f ", upper_output[i].to_double() / (double) INT_MAX); // upper bandpass filter output
+            fprintf(fd_debug, "%7.4f ", lower_output[i].to_double() / (double) INT_MAX); // lower bandpass filter output
+            fprintf(fd_debug, "%7.4f ", upper_levels[i].to_double()); // upper, rectify and RC filter
+            fprintf(fd_debug, "%7.4f ", lower_levels[i].to_double()); // lower, rectify and RC filter
 
             switch(received_bit[i]) {
                 case ZERO:          fprintf(fd_debug, "0 "); break;
