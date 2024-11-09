466194c81b87c73b9cd4f0d1cfdaef337311ec35
Jack Whitham <jack.d.whitham@gmail.com>
2024-11-09 09:59:45 +0000

    reduced

--- run.sh
+++ run.sh
@@ -5,6 +5,6 @@ gcc -o siggen.exe siggen.c -Wall -Werror -g  -O -lm
 g++ -o sigdec.exe sigdec.cpp -std=c++17 -Wall -Werror -g  -O -lm
 ./siggen.exe test_data signal.wav debug_1
 ./sigdec.exe signal.wav output debug_2
-#gnuplot test1.gnuplot > t.png
+gnuplot test1.gnuplot > t.png
 cmp test_data output
 
