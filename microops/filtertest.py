5351b8c05bd5ceb537c0cec2e2ee597e894ac5c6
Jack Whitham <jack.d.whitham@gmail.com>
2024-11-17 17:24:19 +0000

    refactor

--- microops/filtertest.py
+++ microops/filtertest.py
@@ -1,193 +1,21 @@
-import filtersetup
+
 from filtersetup import (
-        ControlLine, ControlLines,
-        OperationList, Register, Operation, ControlOperation,
-        ALL_BITS, A_BITS, R_BITS, make_fixed, make_float,
-        SHIFT_CONTROL_LINE,
+        OperationList, Register, ControlLine,
+        make_fixed, make_float,
         multiply_accumulate, filter_step, demodulator,
         multiply_accumulate_via_regs, move_reg_to_reg,
         set_X_to_abs_O1, set_Y_to_X_minus_reg,
-        move_X_to_L_if_Y_is_not_negative,
+        ALL_BITS, move_X_to_L_if_Y_is_not_negative,
+#        multiply_accumulate, filter_step, demodulator,
+#        multiply_accumulate_via_regs, move_reg_to_reg,
     )
-from settings import FRACTIONAL_BITS, NON_FRACTIONAL_BITS, SAMPLE_RATE
-import enum, math, typing, random, struct
+from execute import run_ops
+from settings import FRACTIONAL_BITS, NON_FRACTIONAL_BITS
+import random, typing, struct
 
 ACCEPTABLE_ERROR = (1.0 / (1 << (FRACTIONAL_BITS - 3)))
 VERY_SMALL_ERROR = (1.0 / (1 << FRACTIONAL_BITS)) * 1.01
 
-class XSelect(enum.Enum):
-    PASSTHROUGH_X = 0
-    PASSTHROUGH_REG_OUT = 1
-    NEGATE_REG_OUT = 2
-    BORROW_REG_OUT = 3
-
-class YSelect(enum.Enum):
-    X_MINUS_REG_OUT = 0
-    BORROW_X_MINUS_REG_OUT = 1
-
-class SpecialRegister(enum.Enum):
-    X_SELECT = -100
-    Y_SELECT = -101
-    MUX_SELECT = -102
-    REPEAT_COUNTER = -103
-
-class NextStep(enum.Enum):
-    NEXT = enum.auto()
-    REPEAT = enum.auto()
-    RESTART = enum.auto()
-
-RegFile = typing.Dict[typing.Union[Register, SpecialRegister], int]
-NUMBER_TO_REGISTER = {r.value: r for r in Register}
-
-def execute(controls: ControlLines, inf: RegFile,
-        reverse_in_values: typing.List[int],
-        out_values: typing.List[int]) -> typing.Tuple[NextStep, RegFile]:
-    outf = dict(inf)
-    mux_select = inf[SpecialRegister.MUX_SELECT]
-    reg_out = inf[NUMBER_TO_REGISTER[mux_select]] & 1
-
-    if ControlLine.ADD_A_TO_R in controls:
-        outf[Register.R] = (inf[Register.R] + inf[Register.A]) & (1 << R_BITS) - 1
-    if ControlLine.BANK_SWITCH in controls:
-        outf[Register.L], outf[Register.LS] = inf[Register.LS], inf[Register.L]
-        outf[Register.O1], outf[Register.O1S] = inf[Register.O1S], inf[Register.O1]
-        outf[Register.O2], outf[Register.O2S] = inf[Register.O2S], inf[Register.O2]
-    if ControlLine.LOAD_I0_FROM_INPUT in controls:
-        outf[Register.I0] = reverse_in_values.pop()
-    if ControlLine.SEND_Y_TO_OUTPUT in controls:
-        out_values.append(inf[Register.Y])
-    if ControlLine.SET_X_IN_TO_X in controls:
-        outf[SpecialRegister.X_SELECT] = XSelect.PASSTHROUGH_X.value
-    if ControlLine.SET_X_IN_TO_REG_OUT in controls:
-        outf[SpecialRegister.X_SELECT] = XSelect.PASSTHROUGH_REG_OUT.value
-    if ControlLine.SET_X_IN_TO_ABS_O1_REG_OUT in controls:
-        if inf[Register.O1] >> (ALL_BITS - 1):
-            outf[SpecialRegister.X_SELECT] = XSelect.NEGATE_REG_OUT.value
-        else:
-            outf[SpecialRegister.X_SELECT] = XSelect.PASSTHROUGH_REG_OUT.value
-    if ControlLine.SET_Y_IN_TO_X_MINUS_REG_OUT in controls:
-        outf[SpecialRegister.Y_SELECT] = YSelect.X_MINUS_REG_OUT.value
-    if ControlLine.ASSERT_X_IS_ABS_O1 in controls:
-        assert abs(make_float(inf[Register.O1])) == make_float(abs(inf[Register.X]))
-    if ControlLine.ASSERT_A_HIGH_ZERO in controls:
-        assert (inf[Register.A] >> ALL_BITS) == 0
-    if ControlLine.ASSERT_A_LOW_ZERO in controls:
-        assert (inf[Register.A] & ((1 << ALL_BITS) - 1)) == 0
-    if ControlLine.ASSERT_R_ZERO in controls:
-        assert inf[Register.R] == 0
-    if ControlLine.ASSERT_Y_IS_X_MINUS_L in controls:
-        assert inf[Register.Y] == ((inf[Register.X] - inf[Register.L]) & ((1 << ALL_BITS) - 1))
-    if ControlLine.SEND_O1_TO_OUTPUT in controls:
-        out_values.append(inf[Register.O1])
-    if ControlLine.SEND_L_TO_OUTPUT in controls:
-        out_values.append(inf[Register.L])
-
-    # Shift for generic registers
-    for (reg, cl) in SHIFT_CONTROL_LINE.items():
-        if cl in controls:
-            outf[reg] = (inf[reg] | (reg_out << ALL_BITS)) >> 1
-
-    # Shift for some registers is special
-    if ControlLine.SHIFT_R_RIGHT in controls:
-        # R register always shift in zero
-        outf[Register.R] = inf[Register.R] >> 1
-    if ControlLine.SHIFT_A_RIGHT in controls:
-        # A register is wider
-        outf[Register.A] = (inf[Register.A] | (reg_out << A_BITS)) >> 1
-    if ControlLine.SHIFT_X_RIGHT in controls:
-        # X register has a special function input (passthrough/abs)
-        if inf[SpecialRegister.X_SELECT] == XSelect.PASSTHROUGH_REG_OUT.value:
-            x_in = reg_out
-        elif inf[SpecialRegister.X_SELECT] == XSelect.PASSTHROUGH_X.value:
-            x_in = inf[Register.X] & 1
-        elif inf[SpecialRegister.X_SELECT] == XSelect.NEGATE_REG_OUT.value:
-            x_in = 4 - reg_out
-            if x_in & 2:
-                outf[SpecialRegister.X_SELECT] = XSelect.BORROW_REG_OUT.value
-        elif inf[SpecialRegister.X_SELECT] == XSelect.BORROW_REG_OUT.value:
-            x_in = 3 - reg_out
-            if not (x_in & 2):
-                outf[SpecialRegister.X_SELECT] = XSelect.NEGATE_REG_OUT.value
-        else:
-            assert False
-        outf[Register.X] = (inf[Register.X] | ((x_in & 1) << ALL_BITS)) >> 1
-    if ControlLine.SHIFT_Y_RIGHT in controls:
-        # Y register has a special function input (subtract)
-        if inf[SpecialRegister.Y_SELECT] == YSelect.X_MINUS_REG_OUT.value:
-            y_in = 4 + (inf[Register.X] & 1) - reg_out
-            if y_in & 2:
-                outf[SpecialRegister.Y_SELECT] = YSelect.BORROW_X_MINUS_REG_OUT.value
-        elif inf[SpecialRegister.Y_SELECT] == YSelect.BORROW_X_MINUS_REG_OUT.value:
-            y_in = 3 + (inf[Register.X] & 1) - reg_out
-            if not (y_in & 2):
-                outf[SpecialRegister.Y_SELECT] = YSelect.X_MINUS_REG_OUT.value
-        else:
-            assert False
-        outf[Register.Y] = (inf[Register.Y] | ((y_in & 1) << ALL_BITS)) >> 1
-
-    if ((ControlLine.SET_MUX_BIT_1 in controls)
-    or (ControlLine.SET_MUX_BIT_2 in controls)
-    or (ControlLine.SET_MUX_BIT_4 in controls)
-    or (ControlLine.SET_MUX_BIT_8 in controls)):
-        outf[SpecialRegister.MUX_SELECT] = 0
-        if ControlLine.SET_MUX_BIT_1 in controls:
-            outf[SpecialRegister.MUX_SELECT] |= 1
-        if ControlLine.SET_MUX_BIT_2 in controls:
-            outf[SpecialRegister.MUX_SELECT] |= 2
-        if ControlLine.SET_MUX_BIT_4 in controls:
-            outf[SpecialRegister.MUX_SELECT] |= 4
-        if ControlLine.SET_MUX_BIT_8 in controls:
-            outf[SpecialRegister.MUX_SELECT] |= 8
-    if ControlLine.SET_MUX_L_OR_X in controls:
-        if inf[Register.Y] >> (ALL_BITS - 1):
-            outf[SpecialRegister.MUX_SELECT] = Register.L.value # Y negative, use L
-        else:
-            outf[SpecialRegister.MUX_SELECT] = Register.X.value # Y non-negative, use X
-    if ControlLine.RESTART in controls:
-        return (NextStep.RESTART, outf)
-    elif ControlLine.REPEAT_FOR_ALL_BITS in controls:
-        outf[SpecialRegister.REPEAT_COUNTER] = (inf[SpecialRegister.REPEAT_COUNTER] + 1) % ALL_BITS
-        if outf[SpecialRegister.REPEAT_COUNTER] != 0:
-            return (NextStep.REPEAT, outf)
-
-    return (NextStep.NEXT, outf)
-
-def run_ops(ops: OperationList, in_values: typing.List[int], debug: bool) -> typing.List[int]:
-    reg_file: RegFile = {}
-    for gr in Register:
-        reg_file[gr] = 0
-    for sr in SpecialRegister:
-        reg_file[sr] = 0
-    op_index = 0
-    out_values: typing.List[int] = []
-    reverse_in_values = list(reversed(in_values))
-    while op_index < len(ops):
-        op = ops[op_index]
-        if isinstance(op, ControlOperation):
-            if debug:
-                print(f"  op: {op_index} {op}")
-            previous_reg_file = reg_file
-            (next_step, reg_file) = execute(op.controls, previous_reg_file, reverse_in_values, out_values)
-            if debug:
-                for r in reg_file.keys():
-                    if reg_file[r] != previous_reg_file[r]:
-                        print(f"   reg {r.name}: {previous_reg_file[r]:08x} -> {reg_file[r]:08x}")
-
-            if next_step == NextStep.RESTART:
-                if len(reverse_in_values) == 0:
-                    return out_values
-                else:
-                    op_index = 0
-            elif next_step == NextStep.NEXT:
-                op_index += 1
-        else:
-            if debug:
-                print(f" {op}")
-            op_index += 1
-
-    # Gone over the end of the program
-    raise Exception("Program must end in RESTART")
-        
 def test_multiply_accumulate(r: random.Random, debug: int, num_multiply_tests: int) -> None:
     print(f"Test multiply accumulate")
     for i in range(num_multiply_tests):
