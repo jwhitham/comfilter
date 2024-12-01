62f7568ed343c0d5a62c3e6c9e4bd0af6e824f58
Jack Whitham <jack.d.whitham@gmail.com>
2024-12-01 13:15:02 +0000

    Hello\r\n

--- microops/pattern_test.py
+++ microops/pattern_test.py
@@ -11,32 +11,38 @@ from fpga_test import (
     )
 import typing
 
-NUM_TEST_BITS = 10
+def out_bit(ops: OperationList, r: Register) -> None:
+    ops.mux(r)
+    ops.add(ControlLine.SET_X_IN_TO_X_AND_CLEAR_Y_BORROW)
+    ops.add(ControlLine.SHIFT_Y_RIGHT, ControlLine.SHIFT_I0_RIGHT)
+    ops.add(ControlLine.SEND_Y_TO_OUTPUT)
 
 def output_pattern_from_input(ops: OperationList) -> None:
     # Load new input
     ops.add(ControlLine.LOAD_I0_FROM_INPUT)
 
-    for i in range(NUM_TEST_BITS):
-        # output LSB[I0] and shift I0 right
-        ops.mux(Register.I0)
-        ops.add(ControlLine.SET_X_IN_TO_X_AND_CLEAR_Y_BORROW)
-        ops.add(ControlLine.SHIFT_Y_RIGHT, ControlLine.SHIFT_I0_RIGHT)
-        ops.add(ControlLine.SEND_Y_TO_OUTPUT)
+    # prepare by zeroing X register
+    ops.mux(Register.ZERO)
+    ops.add(ControlLine.SET_X_IN_TO_REG_OUT)
+    ops.add(ControlLine.SHIFT_X_RIGHT, ControlLine.REPEAT_FOR_ALL_BITS)
 
+    # initial serial line state is 1
+    out_bit(ops, Register.ONE)
+
+    # hold the 1 state for a while
+    ops.add(ControlLine.REPEAT_FOR_ALL_BITS)
+
+    # start bit is 0
+    out_bit(ops, Register.ZERO)
+    # 8 bits starting at LSB
+    for i in range(8):
+        out_bit(ops, Register.I0)
+    # stop bit is 1
+    out_bit(ops, Register.ONE)
+
+    # stay in 1 state
+    ops.add(ControlLine.REPEAT_FOR_ALL_BITS)
     ops.add(ControlLine.RESTART)
-
-def test_output_pattern_from_input() -> None:
-    ops = FPGAOperationList()
-    output_pattern_from_input(ops)
-    in_values = [0x155, 0x299, 0x0fe]
-    out_values = fpga_run_ops(ops, in_values)
-    assert len(out_values) == (len(in_values) * NUM_TEST_BITS)
-    for i in range(len(in_values)):
-        for j in range(NUM_TEST_BITS):
-            actual = out_values[j + (i * NUM_TEST_BITS)] >> (ALL_BITS - 1)
-            expect = (in_values[i] >> j) & 1
-            assert actual == expect
 
 def output_pattern_from_rom(ops: OperationList, pattern: typing.List[int]) -> None:
     # input is not used
@@ -48,36 +54,33 @@ def output_pattern_from_rom(ops: OperationList, pattern: typing.List[int]) -> No
     ops.add(ControlLine.SHIFT_X_RIGHT, ControlLine.REPEAT_FOR_ALL_BITS)
 
     # initial serial line state is 1
-    def out(r) -> None:
-        ops.mux(r)
-        ops.add(ControlLine.SET_X_IN_TO_X_AND_CLEAR_Y_BORROW)
-        ops.add(ControlLine.SHIFT_Y_RIGHT)
-        ops.add(ControlLine.SEND_Y_TO_OUTPUT)
-    out(Register.ONE)
+    out_bit(ops, Register.ONE)
 
     # hold the 1 state for a while
     ops.add(ControlLine.REPEAT_FOR_ALL_BITS)
 
     for data in pattern:
         # start bit is 0
-        out(Register.ZERO)
+        out_bit(ops, Register.ZERO)
         # 8 bits starting at LSB
         for i in range(8):
             if (data >> i) & 1:
-                out(Register.ONE)
+                out_bit(ops, Register.ONE)
             else:
-                out(Register.ZERO)
+                out_bit(ops, Register.ZERO)
         # stop bit is 1
-        out(Register.ONE)
+        out_bit(ops, Register.ONE)
 
     # stay in 1 state
     ops.add(ControlLine.REPEAT_FOR_ALL_BITS)
     ops.add(ControlLine.RESTART)
 
 def main() -> None:
-    test_output_pattern_from_input()
     ops = FPGAOperationList()
-    output_pattern_from_rom(ops, list(b"Hi!"))
+    output_pattern_from_input(ops)
+    ops.generate()
+    ops = FPGAOperationList()
+    output_pattern_from_rom(ops, list(b"Hello\r\n"))
     ops.generate()
 
 
