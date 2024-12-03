7d0eaef4e747114628eb34a86eee162bce5f1cbe
Jack Whitham <jack.d.whitham@gmail.com>
2024-12-03 18:40:14 +0000

    rough draft of test program with UART connection

--- fpga/filter_main.vhdl
+++ fpga/filter_main.vhdl
@@ -13,12 +13,12 @@ entity filter_main is
     port (
         clock_in            : in std_logic;
 
-        test_A3             : out std_logic := '0';
-        test_A2             : out std_logic := '0';
-        test_A1             : out std_logic := '0';
-        test_C3             : out std_logic := '0';
-        test_D3             : out std_logic := '0';
-        test_B1             : out std_logic := '0';
+        test_A3             : out std_logic := '0';  -- serial input
+        test_A2             : out std_logic := '0';  -- serial output
+        test_A1             : out std_logic := '0';  -- state machine READY or LOAD_LOW
+        test_C3             : out std_logic := '0';  -- state machine LOAD_LOW or LOAD_HIGH
+        test_D3             : out std_logic := '0';  -- last serial bit from filter
+        test_B1             : out std_logic := '0';  -- i0 debug out
 
         -- LED outputs
         lcols_out           : out std_logic_vector (3 downto 0) := "0000";
@@ -27,22 +27,9 @@ end filter_main;
 
 architecture structural of filter_main is
 
-
-    -- 4800 = 10kHz -> 2500 baud
-    -- 1250 = 38.4kHz -> 9600 baud
-    constant divider_period     : Natural := 1250;
-
     subtype reset_count_t is Natural range 0 to 15;
     signal reset_count          : reset_count_t := 15;
-    subtype slow_count_t is Natural range 0 to divider_period - 1;
-    signal slow_count           : slow_count_t := 0;
-    signal clock_div            : std_logic := '0';
-    signal clock_2              : std_logic := '0';
     signal reset                : std_logic := '1';
-    signal test_A3_copy         : std_logic := '0';
-    signal test_A2_copy         : std_logic := '0';
-    signal test_C3_copy         : std_logic := '0';
-    signal test_B1_copy         : std_logic := '0';
 
     signal serial_ready         : std_logic := '0';
     signal serial_data          : std_logic := '0';
@@ -50,110 +37,121 @@ architecture structural of filter_main is
     signal input_strobe         : std_logic := '0';
     signal input_ready          : std_logic := '0';
     signal restart_debug        : std_logic := '0';
-    signal data_strobe          : std_logic := '0';
-    signal data_value           : std_logic := '0';
-    signal i0_debug             : std_logic := '0';
 
-    constant byte_array_size    : Natural := 8;
-    subtype byte_t is std_logic_vector(7 downto 0);
-    subtype byte_index_t is Natural range 0 to byte_array_size - 1;
-    type byte_array_t is array (byte_index_t) of byte_t;
-    signal input_index          : byte_index_t := 0;
+    signal uart_data_in         : std_logic_vector (7 downto 0) := (others => '0');
+    signal uart_strobe_in       : std_logic := '0';
+    signal uart_data_out        : std_logic_vector (7 downto 0) := (others => '0');
+    signal uart_strobe_out      : std_logic := '0';
+    signal uart_ready_out       : std_logic := '0';
 
-    component SB_GB
-        port (USER_SIGNAL_TO_GLOBAL_BUFFER:in std_logic;
-                GLOBAL_BUFFER_OUTPUT:out std_logic);
-    end component;
-
-    constant input_rom : byte_array_t :=
-        (0 => x"48",
-         1 => x"65",
-         2 => x"6c",
-         3 => x"6c",
-         4 => x"6f",
-         5 => x"21",
-         6 => x"0d",
-         7 => x"0a");
-
+    type test_state_t is (READY_OUT, LOAD_LOW, LOAD_HIGH, WAIT_RESULT);
+    signal test_state           : test_state_t := READY_OUT;
 begin
     ifu : entity filter_unit
-        port map (clock_in => clock_div,
+        port map (clock_in => clock_in,
                 reset_in => reset,
                 input_strobe_in => input_strobe,
                 input_data_in => input_value,
-                input_ready_out => test_A3_copy,
-                i0_debug_out => i0_debug,
-                restart_debug_out => test_A2_copy,
+                input_ready_out => input_ready,
+                i0_debug_out => test_B1,
+                restart_debug_out => restart_debug,
                 serial_ready_out => serial_ready,
                 serial_data_out => serial_data);
 
-    test_A3 <= test_A3_copy;
-    test_A2 <= test_A2_copy;
-    test_C3 <= test_C3_copy;
-
-    test_A1 <= serial_ready;
-    test_D3 <= i0_debug;
-    test_B1 <= test_B1_copy;
-    lcols_out (0) <= '0';
-    lcols_out (3 downto 1) <= (others => '1');
-    lrows_out (0) <= not reset;
-    lrows_out (1) <= not clock_div;
-    lrows_out (2) <= not test_A3_copy;
-    lrows_out (3) <= not test_A2_copy;
-    lrows_out (4) <= not serial_ready;
-    lrows_out (5) <= not test_C3_copy;
-    lrows_out (6) <= not i0_debug;
-    lrows_out (7) <= not test_B1_copy;
-
-    process (clock_div) is
-    begin
-        if clock_div = '1' and clock_div'event then
-            if serial_ready = '1' then
-                test_C3_copy <= serial_data;
-            end if;
-        end if;
-    end process;
-
-    process (clock_div) is
-    begin
-        if clock_div = '1' and clock_div'event then
-            input_strobe <= '0';
-            input_value <= (others => '0');
-            test_B1_copy <= '0';
-            if test_A3_copy = '1' and input_strobe = '0' then
-                input_strobe <= '1';
-                input_value (7 downto 0) <= input_rom (input_index);
-                if input_index = byte_array_size - 1 then
-                    input_index <= 0;
-                    test_B1_copy <= '1';
-                else
-                    input_index <= input_index + 1;
-                end if;
-            end if;
-        end if;
-    end process;
-
-    clock_buffer : SB_GB
+    u : entity uart 
+        generic map (
+            clock_frequency => 96.0e6,
+            baud_rate => 115200.0)
         port map (
-            USER_SIGNAL_TO_GLOBAL_BUFFER => clock_2, 
-            GLOBAL_BUFFER_OUTPUT => clock_div);
+            data_in => uart_data_in,
+            strobe_in => uart_strobe_in,
+            data_out => uart_data_out,
+            strobe_out => uart_strobe_out,
+            ready_out => uart_ready_out,
+            reset_in => reset,
+            serial_in => test_A3,
+            serial_out => test_A2,
+            clock_in => clock_in);
 
     process (clock_in) is
     begin
         if clock_in = '1' and clock_in'event then
-            if slow_count = 0 then
-                clock_2 <= not clock_2;
-                if clock_2 = '0' then
-                    if reset_count = 0 then
-                        reset <= '0';
-                    else
-                        reset <= '1';
-                        reset_count <= reset_count - 1;
+            uart_strobe_in <= '0';
+            input_strobe <= '0';
+            test_A1 <= '0';
+            test_C3 <= '0';
+            case test_state is
+                when READY =>
+                    test_A1 <= '1';
+                    if uart_strobe_out = '1' then
+                        lrows_out <= not uart_data_out;
+                        if uart_data_out = x"54" then -- T -> begin test
+                            if input_ready = '1' then
+                                uart_data_in <= x"75" -- u -> undefined initial state
+                                test_state <= LOAD_HIGH;
+                            else
+                                uart_data_in <= x"66"; -- f -> fail (not ready)
+                                uart_strobe_in <= '1';
+                            end if;
+                        else
+                            uart_data_in <= uart_data_out; -- echo
+                            uart_strobe_in <= '1';
+                        end if;
                     end if;
-                end if;
-                slow_count <= divider_period - 1;
+                when LOAD_HIGH =>
+                    test_C3 <= '1';
+                    if uart_strobe_out = '1' then
+                        lrows_out <= not uart_data_out;
+                        input_value (15 downto 8) <= uart_data_in;
+                        test_state <= LOAD_LOW;
+                    end if;
+                when LOAD_LOW =>
+                    test_A1 <= '1';
+                    test_C3 <= '1';
+                    if uart_strobe_out = '1' then
+                        lrows_out <= not uart_data_out;
+                        input_value (7 downto 0) <= uart_data_in;
+                        input_strobe <= '1'; -- should start processing
+                        test_state <= WAIT_RESULT;
+                    end if;
+                when WAIT_RESULT =>
+                    input_value <= (others => '0');
+                    if serial_ready = '1' then -- got a bit, shift it
+                        uart_data_in (7 downto 1) <= uart_data_in (6 downto 0);
+                        uart_data_in (0) <= serial_data_out;
+                        lrows_out (7 downto 1) <= not uart_data_in (6 downto 0);
+                        lrows_out (0) <= not serial_data_out;
+                        test_D3 <= serial_data_out;
+                    end if;
+                    if restart_debug = '1' then -- rebooted
+                        lrows_out <= not uart_data_in;
+                        uart_strobe_in <= '1';
+                        test_state <= READY;
+                    end if;
+                    if uart_strobe_out = '1' then -- aborted
+                        lrows_out <= not uart_data_in;
+                        uart_data_in <= x"61"; -- a -> aborted
+                        uart_strobe_in <= '1';
+                        test_state <= READY;
+                    end if;
+            end case;
+            if reset = '1' then
+                test_state <= READY;
+            end if;
+        end if;
+    end process;
+
+    lcols_out (0) <= '0';
+    lcols_out (3 downto 1) <= (others => '1');
+
+    process (clock_in) is
+    begin
+        if clock_in = '1' and clock_in'event then
+            if reset_count = 0 then
+                reset <= '0';
             else
-                slow_count <= slow_count - 1;
+                reset <= '1';
+                reset_count <= reset_count - 1;
             end if;
         end if;
     end process;
---
+++ fpga/uart.vhdl
@@ -0,0 +1,137 @@
+
+library work;
+use work.all;
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity uart is
+    generic (
+        clock_frequency : Real;
+        baud_rate       : Real);
+    port (
+        data_in       : in std_logic_vector (7 downto 0);
+        strobe_in     : in std_logic;
+        data_out      : out std_logic_vector (7 downto 0) := (others => '0');
+        strobe_out    : out std_logic := '0';
+        ready_out     : out std_logic := '0';
+        reset_in      : in std_logic;
+        serial_in     : in std_logic;
+        serial_out    : out std_logic := '0';
+        clock_in      : in std_logic);
+end uart;
+
+architecture structural of uart is
+
+    signal baud_div_16        : std_logic := '0';
+
+begin
+    generate_clock_enable : entity pulse_gen
+        generic map (
+            clock_frequency => clock_frequency,
+            pulse_frequency => baud_rate * 16.0)
+        port map (
+            pulse_out => baud_div_16,
+            clock_in => clock_in);
+
+    serial_input : block
+        signal serial_in_reg : std_logic := '0';
+        signal data          : std_logic_vector (7 downto 0) := (others => '0');
+
+        subtype t_receive_state is Natural range 16#000# to 16#0ff#;
+        signal receive_state : t_receive_state := 0;
+    begin
+        data_out <= data;
+
+        process (clock_in)
+        begin
+            if clock_in'event and clock_in = '1' then
+                strobe_out <= '0';
+                serial_in_reg <= serial_in;
+
+                if baud_div_16 = '1' then
+                    if receive_state /= 0 or serial_in_reg = '0' then
+                        -- receiving bits
+                        receive_state <= receive_state + 1;
+                        case receive_state is
+                            when 16#008# =>
+                                -- start bit: should be 0. If not, discard packet and reset
+                                if serial_in_reg /= '0' then
+                                    receive_state <= 0;
+                                end if;
+                            when 16#018# | 16#028# | 16#038# | 16#048#
+                                    | 16#058# | 16#068# | 16#078# | 16#088# =>
+                                -- data bit: shift into data register
+                                data (6 downto 0) <= data (7 downto 1);
+                                data (7) <= serial_in_reg;
+                            when 16#098# =>
+                                -- stop bit: should be 1. If not, discard packet and reset.
+                                if serial_in_reg = '1' then
+                                    strobe_out <= '1';
+                                end if;
+                                receive_state <= 0;
+                            when others =>
+                                null;
+                        end case;
+                    end if;
+                end if;
+            end if;
+        end process;
+    end block serial_input;
+
+    serial_output : block
+        signal data          : std_logic_vector (7 downto 0) := (others => '0');
+
+        subtype t_send_state is Natural range 16#000# to 16#0ff#;
+        signal send_state   : t_send_state := 0;
+    begin
+        process (clock_in)
+        begin
+            if clock_in'event and clock_in = '1' then
+
+                ready_out <= '0';
+
+                if reset_in = '1' then
+                    -- send a break (hold serial out low)
+                    serial_out <= '0';
+                    send_state <= 0;
+                    
+                elsif send_state = 0 then
+                    if strobe_in = '1' then
+                        -- begin sending
+                        data <= data_in;
+                        send_state <= 1;
+                    else
+                        -- ready to send
+                        ready_out <= '1';
+                    end if;
+                    serial_out <= '1';
+                else
+                    if baud_div_16 = '1' then
+                        -- send data
+                        send_state <= send_state + 1;
+                        case send_state is
+                            when 16#001# =>
+                                -- start bit
+                                serial_out <= '0';
+                            when 16#011# | 16#021# | 16#031# | 16#041#
+                                    | 16#051# | 16#061# | 16#071# | 16#081# =>
+                                -- data bit
+                                serial_out <= data (0);
+                                data (6 downto 0) <= data (7 downto 1);
+                            when 16#091# =>
+                                -- stop bit
+                                serial_out <= '1';
+                            when 16#0a0# =>
+                                -- finished
+                                send_state <= 0;
+                            when others =>
+                                null;
+                        end case;
+                    end if;
+                end if;
+            end if;
+        end process;
+    end block serial_output;
+
+end architecture structural;
